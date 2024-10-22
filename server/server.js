f33f4f116d06f5a2b7485fdd624e952ad5177759
Tom Pinataro <tompinataro@gmail.com>
2024-10-22 10:53:56 -0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 5001;
+
+// Middleware Includes
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
+
+// Route Includes
+const userRouter = require('./routes/user.router');
+const shelfRouter = require('./routes/shelf.router');
+
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('build'));
+
+// Passport Session Configuration
+app.use(sessionMiddleware);
+
+// Start Passport Sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/api/user', userRouter);
+app.use('/api/shelf', shelfRouter);
+
+// Listen Server & Port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
